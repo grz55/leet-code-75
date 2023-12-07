2add470bcd56e6258013372b60a6671f58a0f9b0
grz55 <grz55@vp.pl>
2023-12-07 17:17:57 +0100

    Contains Duplicate other solutions explanation added

--- src/main/java/pl/grz55/arrays/ContainsDuplicate.java
+++ src/main/java/pl/grz55/arrays/ContainsDuplicate.java
@@ -4,8 +4,12 @@ public class ContainsDuplicate {
 
     // https://leetcode.com/problems/contains-duplicate/
 
-    //  Given an integer array nums, return true if any value appears at least twice in the array,
+    // Given an integer array nums, return true if any value appears at least twice in the array,
     // and return false if every element is distinct.
+
+    // Other solutions:
+    // - Arrays.sort() the array and check equality of adjacent numbers
+    // - Adding numbers to HashSet and checking if added number exists
 
     public boolean containsDuplicate(int[] numbers) {
         if (numbers.length == 0 || numbers.length == 1) {
